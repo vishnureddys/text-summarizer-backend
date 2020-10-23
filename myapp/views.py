3751150e9e24e4346ea8c75be3f29e147ca87583
Vishnu S Reddy <vishnusreddy007@gmail.com>
2020-10-24 00:53:35 +0530

    Getting data from wikipedia

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render,redirect
 from .forms import InputForm 
+import wikipedia
 
 # Create your views here. 
 def home_view(request):
@@ -10,13 +11,14 @@ def home_view(request):
         form = InputForm(request.POST)
         if form.is_valid():
             article_name = form.cleaned_data['article_name']
+            get_data_from_wiki(article_name)
             return redirect('success')
     return render(request, 'home.html',{'form':form})
 
 def successView(request):
     return render(request, 'temp.html')
-'''
-	context ={} 
-	context['form']= InputForm() 
-	return render(request, "home.html", context) 
-'''
+
+def get_data_from_wiki(name):
+    data = wikipedia.page(name)
+    print(data.content)
+    return
